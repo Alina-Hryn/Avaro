fa8646decd6f36f817f88067cde8504c242746e7
Alina Hryn <alin.grin2000@gmail.com>
2020-12-05 05:21:42 +0200

    grouped sites

--- avaro/movie/movie/spiders/oskar_smart.py
+++ avaro/movie/movie/spiders/oskar_smart.py
@@ -3,8 +3,8 @@ import scrapy
 
 class OskarSmartSpider(scrapy.Spider):
     name = 'oskar_smart'
-    allowed_domains = ['https://oskar.kyiv.ua/smart/poster']
-    start_urls = ['http://https://oskar.kyiv.ua/smart/poster/']
+    start_urls = ['https://oskar.kyiv.ua/smart/poster/',
+                  'https://oskar.kyiv.ua/gulliver/poster/']
 
     def parse(self, response):
         # response.css("div.main-gallery__name").css("a").xpath("@href").extract()
