7dd266ea34e839577989f1b2b32a8dd255da9dce
anh-honcharuk <anh.honcharuk@gmail.com>
2021-01-23 18:41:25 +0200

    *

--- avaro/movie/scraper/settings.py
+++ avaro/movie/scraper/settings.py
@@ -75,7 +75,7 @@ ROBOTSTXT_OBEY = False
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     'movie.scraper.pipelines.MoviePipeline': 300,
-    'movie.scraper.pipelines.CinemaMoviePipeline': 100,
+    #'movie.scraper.pipelines.CinemaMoviePipeline': 100,
     # 'scraper.pipelines.': 300,
 }
 
